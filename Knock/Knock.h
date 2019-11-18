9b7e4d9ace27c6bef2e75c7416b7cb7e993d5aac
chinna512 <chinnababu.2k14@gmail.com>
2019-11-18 15:28:35 +0530

    Initial Commit

---
+++ Knock/Knock.h
@@ -0,0 +1,19 @@
+//
+//  Knock.h
+//  Knock
+//
+//  Created by Chinnababu Kamanuri on 18/11/19.
+//  Copyright © 2019 Chinnababu Kamanuri. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Knock.
+FOUNDATION_EXPORT double KnockVersionNumber;
+
+//! Project version string for Knock.
+FOUNDATION_EXPORT const unsigned char KnockVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Knock/PublicHeader.h>
+
+
